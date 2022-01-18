3c53a1a816b95b97f60b3faaba5d0682228155ce
Lindsay <lbtannahill@gmail.com>
2022-01-17 19:30:31 -0800

    more refactoring

--- eqArrays.js
+++ eqArrays.js
@@ -17,8 +17,4 @@ const eqArrays = function(array1, array2) {
     }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
+module.exports = eqArrays;
